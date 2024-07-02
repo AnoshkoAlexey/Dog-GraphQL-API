ce86cf6e24ebb72497c49fc96bb430783c65a460
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-07-02 15:33:58 -0700

    Initial commit

---
+++ src/test/java/com/udacity/Dog/GraphQL/API/DogGraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.Dog.GraphQL.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
